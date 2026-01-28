6e40a58d8e604b36bfc1821552dff1396f204e82
th-release <cth.release@gmail.com>
2026-01-28 12:45:45 +0900

    style: rma

--- Rma.go
+++ Rma.go
@@ -1,6 +1,6 @@
 package tradingtools
 
-// Rma calculates Running Moving Average (Wilder's smoothing)
+// Rma calculates Running Moving Average (Wilder's smoothing).
 func (t *Tool) Rma(source []float64, length int) []float64 {
 	alpha := 1.0 / float64(length)
 	return t.expMovingAvg(source, alpha)
